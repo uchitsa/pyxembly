5ff20e0ecbbd53332a08e94f81acbca851539a6a
uchitsa <uchitsa@ya.ru>
2024-03-13 11:59:48 +0300

    update strict

---
+++ strict.py
@@ -0,0 +1,11 @@
+from xembly.base import Base
+
+
+class Strict(Base):
+    def __init__(self, count):
+        self.count = int(count)
+
+    def exec(self, _, cursor):
+        if len(cursor) != self.count:
+            raise Exception(f"there are {len(cursor)} nodes, while {self.count} expected")
+        return cursor
