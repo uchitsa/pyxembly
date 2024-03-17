38540b85c20c92044b685761df1922ace54c9291
uchitsa <uchitsa@ya.ru>
2024-03-17 13:00:00 +0300

    update test

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,6 +1,21 @@
+import unittest
 from unittest import TestCase
+from lxml import etree
+
+from add import Add
 
 
 class TestAdd(TestCase):
     def test_exec(self):
         self.fail()
+
+    def test_add(self):
+        dom = etree.fromstring('<books/>')
+        new_node = etree.Element('book')
+        dom.append(new_node)
+        Add.__init__('book').exec(dom, [dom.xpath('/*').first])
+        self.assertEqual(etree.tostring(dom), b'<books><book></books>')
+
+
+if __name__ == '__main__':
+    unittest.main()
